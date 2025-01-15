4cb1ccf115daed96f85c9ae402fb369f941bab60
Lena Suarez <102402197+Lenas25@users.noreply.github.com>
2025-01-15 00:33:51 -0500

    finish backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,8 +28,8 @@ import { ImagesModule } from './images/images.module';
     ActivityModule,
     GradeModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
+      type: 'postgres',
+      host: process.env.DB_HOST,
       port: process.env.DB_PORT ? Number.parseInt(process.env.DB_PORT) : 3306,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
